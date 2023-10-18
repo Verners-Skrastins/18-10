91265331b117bd9bba1210d499117b7e83543925
Verners-Skrastis <verners.skrastins@grobinasskola.lv>
2023-10-18 14:27:12 +0300

    first commit

--- darbs.py
+++ darbs.py
@@ -1,3 +1,23 @@
 def iegut_datus(fails):
     dati = open(fails, 'r')
-    
+    skaitli = dati.read()
+    return skaitli.split(' ')
+dati = iegut_datus('skaitli.txt')
+
+def saskaitit(skaitli):
+    rezultats = 0
+    for i in skaitli:
+        rezultats += int(i)
+    return rezultats
+def skaitlu_skaits(skaitli):
+    rezultats = 0
+    for i in skaitli:
+        rezultats += 1
+    return rezultats
+def videjais(skaitli):
+    summa = saskaitit(skaitli)
+    skaits = skaitlu_skaits(skaitli)
+    rezultats = summa // skaits
+    return rezultats
+print("Summa ir ", saskaitit(dati), '. Kopā ir ', skaitlu_skaits(dati),' skaitļi. To aritmētiskais vidējais ir ', videjais(dati))
+
